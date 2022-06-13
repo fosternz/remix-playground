39e6ed7e50aa5ed4d2885a34eae3fd23e317b15a
Luke Foster <fosternz@gmail.com>
2022-06-13 22:45:50 +1200

    Add prisma, seed data, fetch list.

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,7 +1,19 @@
-import { LinksFunction } from "@remix-run/node";
-import { Outlet } from "@remix-run/react";
+import { json, LinksFunction, LoaderFunction } from "@remix-run/node";
+import { Link, Outlet, useLoaderData } from "@remix-run/react";
 
 import stylesUrl from '../styles/jokes.css';
+
+import { db } from "~/utils/db.server";
+import { Joke } from "@prisma/client";
+
+type LoaderData = { jokes: Array<Joke> };
+
+export const loader: LoaderFunction = async () => {
+    const data: LoaderData = {
+      jokes: await db.joke.findMany(),
+    };
+    return json(data);
+  };
 
 export const links: LinksFunction = () => {
   return [
@@ -12,13 +24,45 @@ export const links: LinksFunction = () => {
 }
 
 export default function JokesRoute() {
-  return (
-    <div>
-      <h1>J🤪KES</h1>
-      <main>
-        <Outlet />
-      </main>
-    </div>
-  );
+    const data = useLoaderData<LoaderData>();
+    return (
+        <div className="jokes-layout">
+          <header className="jokes-header">
+            <div className="container">
+              <h1 className="home-link">
+                <Link
+                  to="/"
+                  title="Remix Jokes"
+                  aria-label="Remix Jokes"
+                >
+                  <span className="logo">🤪</span>
+                  <span className="logo-medium">J🤪KES</span>
+                </Link>
+              </h1>
+            </div>
+          </header>
+          <main className="jokes-main">
+            <div className="container">
+              <div className="jokes-list">
+                <Link to=".">Get a random joke</Link>
+                <p>Here are a few more jokes to check out:</p>
+                <ul>
+                  {data.jokes.map((joke) => (
+                    <li key={joke.id}>
+                      <Link to={joke.id}>{joke.name}</Link>
+                    </li>
+                  ))}
+                </ul>
+                <Link to="new" className="button">
+                  Add your own
+                </Link>
+              </div>
+              <div className="jokes-outlet">
+                <Outlet />
+              </div>
+            </div>
+          </main>
+        </div>
+      );
 }
 
